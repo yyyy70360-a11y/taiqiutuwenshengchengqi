181782ed134f8dba1f62ca54bddbd10180a52cf2
yyyy70360-a11y <yyyy70360-a11y@users.noreply.github.com>
2026-08-19 06:17:01 +0800

    fix: embed billiards app icon on Windows

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,6 @@
 fn main() {
+    println!("cargo:rerun-if-changed=icons/icon.ico");
+    println!("cargo:rerun-if-changed=icons/icon.png");
+    println!("cargo:rerun-if-changed=icons/icon.icns");
     tauri_build::build()
 }
